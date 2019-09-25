43d9dc741d9ae34c96143b60be6f33bef686a54d
jopana <jopana@Copito22>
2019-09-25 15:03:13 +0200

    Version 0 de Hola Mundo

--- Prac2IS2/src/prac2is2/HolaMundo.java
+++ Prac2IS2/src/prac2is2/HolaMundo.java
@@ -1,14 +1,15 @@
+package prac2is2;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prac2is2;
 
 /**
  *
  * @author jopana
  */
-public class Moto {
+public class HolaMundo {
     
 }
